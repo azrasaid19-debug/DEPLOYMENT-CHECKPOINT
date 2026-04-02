293f181dad9b7e9880c6672df390ef8fd175baa0
azrasaid19-debug <azrasaid19@gmail.com>
2026-04-02 19:34:32 +0300

    Serve React frontend in Express

--- server.js
+++ server.js
@@ -61,3 +61,12 @@ app.get("*", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+const path = require("path");
+
+// Serve frontend
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
