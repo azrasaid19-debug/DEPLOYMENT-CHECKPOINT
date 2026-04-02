c56c81bd7c4d0775877a03cf863ec7b626a1d0cf
azrasaid19-debug <azrasaid19@gmail.com>
2026-04-02 19:42:18 +0300

    Fixed duplicate variables

--- server.js
+++ server.js
@@ -62,8 +62,6 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
-const path = require("path");
-
 // Serve frontend
 app.use(express.static(path.join(__dirname, "client/build")));
 
